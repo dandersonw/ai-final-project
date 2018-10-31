8eceb24bbb460e0e99ea2f1917773bf3d5cbc0e4
zackr <reiter.z@husky.neu.edu>
2018-10-31 16:34:57 -0400

    finished proof of concept

--- magicScrapper.py
+++ magicScrapper.py
@@ -5,11 +5,15 @@ with open('guildsOfRavnica.json') as f:
     data = json.load(f)
 
 cards = data['cards']
-for card in cards:
+# proof that all cards in the set are being loaded
+#for card in cards:
     #print(card['name'])
 
 firstCard = cards[0]
 
-foundCard = srython.cards.Names(firstCard['name'])
+# proof that the scrython sdk works
+foundCard = scrython.cards.Named(exact=firstCard['name'])
 
-print(foundCard)
+# an example of getting an art_crop image uri
+# will be used to fetch pictures for training the model
+print(foundCard.image_uris()['art_crop'])
