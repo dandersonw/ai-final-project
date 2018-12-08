f0287168e77d871355ce40e1f80596a8f5e39032
zackr <reiter.z@husky.neu.edu>
2018-12-08 17:35:59 -0500

    uncommented convert statements because we're about to run

--- data/convert_to_dataset.py
+++ data/convert_to_dataset.py
@@ -29,23 +29,23 @@ def convert_data(output_path, status, sql):
             print("wrote", counter, "of", data_size)
             counter += 1
 
-# dev_sql = "select * from dev_card_table where experiment_status = %s"
-# print("starting dev")
-# convert_data("./tfrecord/dev_testing_data.tfrecord", "TESTING", dev_sql)
-# convert_data("./tfrecord/dev_training_data.tfrecord", "TRAINING", dev_sql)
-# convert_data("./tfrecord/dev_validation_data.tfrecord", "VALIDATION", dev_sql)
-#
-# print("starting total")
+dev_sql = "select * from dev_card_table where experiment_status = %s"
+print("starting dev")
+convert_data("./tfrecord/dev_testing_data.tfrecord", "TESTING", dev_sql)
+convert_data("./tfrecord/dev_training_data.tfrecord", "TRAINING", dev_sql)
+convert_data("./tfrecord/dev_validation_data.tfrecord", "VALIDATION", dev_sql)
+
+print("starting total")
 total_sql = "select * from unique_card_data where experiment_status = %s"
-# convert_data("./tfrecord/testing_data.tfrecord", "TESTING", total_sql)
-# convert_data("./tfrecord/training_data.tfrecord", "TRAINING", total_sql)
-# convert_data("./tfrecord/validation_data.tfrecord", "VALIDATION", total_sql)
-#
-# print("starting modern")
-# modern_sql = total_sql + " and set_name in (select setCode from sets where releaseDate >= date(\"2008-10-03\"))"
-# convert_data("./tfrecord/modern_training_data.tfrecord", "TRAINING", modern_sql)
-# convert_data("./tfrecord/modern_validation_data.tfrecord", "VALIDATION", modern_sql)
-# convert_data("./tfrecord/modern_testing_data.tfrecord", "TESTING", modern_sql)
+convert_data("./tfrecord/testing_data.tfrecord", "TESTING", total_sql)
+convert_data("./tfrecord/training_data.tfrecord", "TRAINING", total_sql)
+convert_data("./tfrecord/validation_data.tfrecord", "VALIDATION", total_sql)
+
+print("starting modern")
+modern_sql = total_sql + " and set_name in (select setCode from sets where releaseDate >= date(\"2008-10-03\"))"
+convert_data("./tfrecord/modern_training_data.tfrecord", "TRAINING", modern_sql)
+convert_data("./tfrecord/modern_validation_data.tfrecord", "VALIDATION", modern_sql)
+convert_data("./tfrecord/modern_testing_data.tfrecord", "TESTING", modern_sql)
 
 print("starting legacy")
 legacy_sql = total_sql + " and set_name in (select setCode from sets where releaseDate < date(\"2008-10-03\"))"
