b5463291a172c2fa17ea2c4fc914443c2bec7572
zackr <reiter.z@husky.neu.edu>
2018-12-08 16:06:42 -0500

    tweaks to image train script to make it work

--- data/convert_to_dataset.py
+++ data/convert_to_dataset.py
@@ -29,46 +29,26 @@ def convert_data(output_path, status, sql):
             print("wrote", counter, "of", data_size)
             counter += 1
 
-dev_sql = "select * from dev_card_table where experiment_status = %s"
-<<<<<<< HEAD
-print("starting dev")
-=======
->>>>>>> 6efae3d23247906a71ed2b67279fb4044a0a9e34
-convert_data("./tfrecord/dev_testing_data.tfrecord", "TESTING", dev_sql)
-convert_data("./tfrecord/dev_training_data.tfrecord", "TRAINING", dev_sql)
-convert_data("./tfrecord/dev_validation_data.tfrecord", "VALIDATION", dev_sql)
-
-<<<<<<< HEAD
-print("starting total")
+# dev_sql = "select * from dev_card_table where experiment_status = %s"
+# print("starting dev")
+# convert_data("./tfrecord/dev_testing_data.tfrecord", "TESTING", dev_sql)
+# convert_data("./tfrecord/dev_training_data.tfrecord", "TRAINING", dev_sql)
+# convert_data("./tfrecord/dev_validation_data.tfrecord", "VALIDATION", dev_sql)
+#
+# print("starting total")
 total_sql = "select * from unique_card_data where experiment_status = %s"
-convert_data("./tfrecord/testing_data.tfrecord", "TESTING", total_sql)
-convert_data("./tfrecord/training_data.tfrecord", "TRAINING", total_sql)
-convert_data("./tfrecord/validation_data.tfrecord", "VALIDATION", total_sql)
-
-print("starting modern")
-modern_sql = total_sql + " and set_name in (select setCode from sets where releaseDate >= date(\"2008-10-03\"))"
-convert_data("./tfrecord/modern_training_data.tfrecord", "TRAINING", modern_sql)
-convert_data("./tfrecord/modern_validation_data.tfrecord", "VALIDATION", modern_sql)
-convert_data("./tfrecord/modern_testing_data.tfrecord", "TESTING", modern_sql)
-
-print("starting legacy")
-legacy_sql = total_sql = " and set_name in (select setCode from sets where releaseDate < date(\"2008-10-03\"))"
-convert_data("./tfrecord/legacy_training_data.tfrecord", "TRAINING", legacy_sql)
-convert_data("./tfrecord/legacy_validation_data.tfrecord", "VALIDATION", legacy_sql)
-convert_data("./tfrecord/legacy_testing_data.tfrecord", "TESTING", legacy_sql)
-=======
-#total_sql = "select * from unique_card_data where experiment_status = %s"
-#convert_data("./tfrecord/testing_data.tfrecord", "TESTING", total_sql)
-#convert_data("./tfrecord/training_data.tfrecord", "TRAINING", total_sql)
-#convert_data("./tfrecord/validation_data.tfrecord", "VALIDATION", total_sql)
-
+# convert_data("./tfrecord/testing_data.tfrecord", "TESTING", total_sql)
+# convert_data("./tfrecord/training_data.tfrecord", "TRAINING", total_sql)
+# convert_data("./tfrecord/validation_data.tfrecord", "VALIDATION", total_sql)
+#
+# print("starting modern")
 # modern_sql = total_sql + " and set_name in (select setCode from sets where releaseDate >= date(\"2008-10-03\"))"
 # convert_data("./tfrecord/modern_training_data.tfrecord", "TRAINING", modern_sql)
 # convert_data("./tfrecord/modern_validation_data.tfrecord", "VALIDATION", modern_sql)
 # convert_data("./tfrecord/modern_testing_data.tfrecord", "TESTING", modern_sql)
 
-# legacy_sql = total_sql = " and set_name in (select setCode from sets where releaseDate < date(\"2008-10-03\"))"
-# convert_data("./tfrecord/legacy_training_data.tfrecord", "TRAINING", legacy_sql)
-# convert_data("./tfrecord/legacy_validation_data.tfrecord", "VALIDATION", legacy_sql)
-# convert_data("./tfrecord/legacy_testing_data.tfrecord", "TESTING", legacy_sql)
->>>>>>> 6efae3d23247906a71ed2b67279fb4044a0a9e34
+print("starting legacy")
+legacy_sql = total_sql + " and set_name in (select setCode from sets where releaseDate < date(\"2008-10-03\"))"
+convert_data("./tfrecord/legacy_training_data.tfrecord", "TRAINING", legacy_sql)
+convert_data("./tfrecord/legacy_validation_data.tfrecord", "VALIDATION", legacy_sql)
+convert_data("./tfrecord/legacy_testing_data.tfrecord", "TESTING", legacy_sql)
--- scripts/train_image_model.py
+++ scripts/train_image_model.py
@@ -4,7 +4,7 @@ import argparse
 import importlib
 import sys
 
-MY_SRC = './src/'
+MY_SRC = '../src/'
 sys.path.append(MY_SRC)
 
 from image_model import densenet121_model
@@ -17,14 +17,8 @@ def main():
     parser.add_argument('--train', required=True)
     parser.add_argument('--valid', required=True)
     parser.add_argument('--test', required=True)
-    parser.add_argument('--config_dir', required=True)
-    parser.add_argument('--config_name', required=True)
     parser.add_argument('--checkpoint_path', required=True)
     args = parser.parse_args()
-
-    sys.path.append(args.config_dir)
-    config_module = importlib.import_module(args.config_name)
-    config = config_module.config
 
     features = {'image'}
     with tf.Session() as sess:
@@ -32,11 +26,11 @@ def main():
         validation_data = data.preload_dataset(args.valid, sess, features=features)
         testing_data = data.preload_dataset(args.test, sess, features=features)
 
-    model = train_model(config, training_data, validation_data, args.checkpoint_path)
+    model = train_model(training_data, validation_data, args.checkpoint_path)
     model.evaluate(x=testing_data[0]['image'], y=testing_data[1])
 
 
-def train_model(config, training_data, validation_data, checkpoint_path):
+def train_model(training_data, validation_data, checkpoint_path):
     model = densenet121_model(img_rows=IMAGE_DIMS[0], img_cols=IMAGE_DIMS[1], color_type=IMAGE_DIMS[2], num_classes=NUM_CLASSES)
     early_stopping = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                         save_best_only=True,
@@ -52,13 +46,7 @@ def train_model(config, training_data, validation_data, checkpoint_path):
                     'validation_data': (validation_data[0]['image'], validation_data[1]),
                     'callbacks': [early_stopping]}
 
-    if config.use_pretrained_embeddings:
-        model.fit(**train_kwargs, epochs=10)
-        model.embedding_layer.trainable = True
-        model.compile(**compilation_kwargs)
-        model.fit(**train_kwargs, epochs=30)
-    else:
-        model.fit(**train_kwargs, epochs=2)
+    model.fit(**train_kwargs, epochs=2)
 
     model.load_weights(checkpoint_path)
     return model
